55b0d9c6cbcb9f0bfcc7a51fbf0b25dfd02b810a
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-11 14:32:36 -0400

    add create a post text

--- phase2project/src/components/Feed.js
+++ phase2project/src/components/Feed.js
@@ -4,12 +4,13 @@ import { Redirect } from "react-router-dom";
 
 function Feed({posts, isLoggedIn}) {
 
+  console.log(posts)
 
   if (!isLoggedIn) return <Redirect exact to="/" />;
 
   return (
     <div>
-        <PhotosList posts={posts}/>
+        {posts.length === 0 ? <h1 style={{color: 'white'}} >Create your first post!</h1> : <PhotosList posts={posts}/> }
     </div>
   )
 }
